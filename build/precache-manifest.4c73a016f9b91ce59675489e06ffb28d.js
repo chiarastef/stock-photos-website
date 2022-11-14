78d34eccad1a92cfe3f861d2bf3e245d1aa84f8f
Chiara Stefanelli <chiarastefanelli27@gmail.com>
2022-11-14 17:38:58 +0200

    added build folder

---
+++ build/precache-manifest.4c73a016f9b91ce59675489e06ffb28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f16464ccaddc5a2139e34e471a1b74e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "484b411aea7fe93a0794",
+    "url": "/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "833aaddd2aad21d82868",
+    "url": "/static/js/2.f9fac789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9fac789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484b411aea7fe93a0794",
+    "url": "/static/js/main.58e6cab9.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
